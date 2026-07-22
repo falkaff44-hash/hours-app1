447e43ef430de0aa7c855cb592169e7ef3e702d6
falkaff44-hash <falkaff44@gmail.com>
2026-07-22 12:32:49 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TimeTracker from "./TimeTracker.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <TimeTracker />
-  </React.StrictMode>
-);
